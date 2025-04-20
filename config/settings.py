39a0f31f2be8fae26ab8ee258886dd14a54598d2
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-04-20 21:56:45 +0300

    feature 1

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
@@ -112,7 +111,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
